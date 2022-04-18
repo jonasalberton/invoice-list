966179c2c930b8ec5677aae34b9a53ccbfb44b4b
jonasalberton <jonasceolin@gmail.com>
2022-04-18 20:53:06 -0300

    chore: Invoice model

--- src/models/Invoice.ts
+++ src/models/Invoice.ts
@@ -1,6 +1,32 @@
+export interface Addreas {
+  street: string,
+  city: string,
+  postCode: string,
+  country: string,
+}
+
+export interface Client {
+  name: string,
+  email: string,
+  addres: Addreas
+}
+
+export interface InvoiceItem {
+  id: string,
+  name: string,
+  qty: number,
+  price: number
+}
+
+
 export interface Invoice  {
   id: number,
-  title: string
+  title: string,
+  addreas: Addreas,
+  client: Client,
+  invoiceDate: Date,
+  paymentTerms: string,
+  items: InvoiceItem[]
 }
 
 export enum InvoiceStatus {
